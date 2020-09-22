d9188d3d8d2360dbe974ceccdf3b2224f979fdfe
Jing Jiang <jasperjiang97@gmail.com>
2020-09-22 02:44:36 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, SafeAreaView} from 'react-native';
 
 import CourseList from './components/CourseList'
 
-const schedule = {
-  title: "CS Course Scheduler 2020-2021",
-  courses: [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }]
-};
-
-
 const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
+  <Text style={styles.bannerStyle}>{title||'[loading...]'}</Text>
 );
 
 const App = () => {
+  const [schedule, setSchedule] = useState({title: '', courses: []});
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title}/>
