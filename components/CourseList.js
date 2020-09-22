fdfd28b00ed6ae9c0fcb32530575205e2833fe1f
Jing Jiang <jasperjiang97@gmail.com>
2020-09-22 04:15:47 -0500

    course conflicts

--- components/CourseList.js
+++ components/CourseList.js
@@ -3,13 +3,8 @@ import {StyleSheet, ScrollView, View} from 'react-native';
 
 import Course from './Course'
 import TermSelector from './TermSelector'
-
-const termMap = {F: 'Fall', W: 'Winter', S: 'Spring'};
-const terms = Object.values(termMap);
-
-const getCourseTerm = course => (
-    termMap[course.id.charAt(0)]
-);
+import CourseSelector from './CourseSelector'
+import {getCourseTerm} from '../utils/course'
 
 const CourseList = ({courses}) => {
     const [selectedTerm, setSelectedTerm] = useState('Spring');
@@ -17,31 +12,14 @@ const CourseList = ({courses}) => {
     const termCourses = courses.filter(course=>selectedTerm === getCourseTerm(course));
 
     return (
-    <ScrollView style={styles.courseList}>
+    <ScrollView>
         <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-        {termCourses.map(course => <Course key={course.id} course={course} />)}
+        <CourseSelector courses={termCourses} />
     </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    courseButton: {
-        flex: 1,
-        borderRadius: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-        height: 60,
-        padding: 10,
-        minWidth: 90,
-        maxWidth: 90,
-        backgroundColor: '#66b0ff',
-    },
-    courseText:{
-        color: '#fff',
-        fontSize: 12,
-        textAlign: 'center',
-    },
 });
 
 export default CourseList;
--- components/TermSelector.js
+++ components/TermSelector.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
 
-const termMap = {F: 'Fall', W: 'Winter', S: 'Spring'};
-const terms = Object.values(termMap);
+import {terms} from '../utils/course'
 
 const TermButton = ({term, setSelectedTerm, isActive}) => (
     <TouchableOpacity style={styles[isActive ? 'termButtonActive' : 'termButton']} onPress={() => setSelectedTerm(term)}>
