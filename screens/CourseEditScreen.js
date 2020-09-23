bdf90e37c9117c3093c850f286957bd50b526a0e
Jing Jiang <jasperjiang97@gmail.com>
2020-09-22 23:34:35 -0500

    database store

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, SafeAreaView, ScrollView,Text} from 'react-native';
 import Form from '../expo-form-starter-main/Form';
 import * as Yup from 'yup';
+import {firebase} from '../firebase'
 
 const validationSchema = Yup.object().shape({
   id: Yup.string()
@@ -17,24 +18,24 @@ const validationSchema = Yup.object().shape({
     .label('Title'),
 });
 
-
-const Field = ({label, value}) => {
-  return (
-    <View style={styles.fieldContainer}>
-        <Text style={styles.label}>{label}</Text>
-        <Text style={styles.field}>{value}</Text>
-    </View>
-  );
-};
-
 const CourseEditScreen = ({navigation, route}) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title} = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Form initialValues={{id:course.id, meets:course.meets, title:course.title,}}
-          validationSchema={validationSchema}>
+          validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}>
           <Form.Field
             name="id"
             leftIcon="identifier"
@@ -53,6 +54,8 @@ const CourseEditScreen = ({navigation, route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={'Update'} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
