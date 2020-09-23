02613b2155a3b421c75d0b43b87f0105a39a2310
Jing Jiang <jasperjiang97@gmail.com>
2020-09-22 22:23:40 -0500

    edit screen

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import { StyleSheet, Text, SafeAreaView} from 'react-native';
 
+import UserContext from '../UserContext'
 import CourseList from '../components/CourseList'
+import CourseEditScreen from './CourseEditScreen'
 
 const Banner = ({title}) => (
   <Text style={styles.bannerStyle}>{title||'[loading...]'}</Text>
@@ -12,8 +14,11 @@ const ScheduleScreen = ({navigation}) => {
   const [schedule, setSchedule] = useState({title: '', courses: []});
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
+  const user = useContext(UserContext);
+  const canEdit = user && user.role === 'admin';
+
   const view = (course) => {
-    navigation.navigate('CourseDetailScreen', {course});
+    navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
 
   useEffect(() => {
